779f0101ddd4b7b643916913af44b4de59e823c3
jhoneber31 <jhoneber31@gmail.com>
2026-04-15 12:00:00 -0500

    Commit 2: Esqueleto del agente y logica de navegacion basica

---
+++ mi_agente.py
@@ -0,0 +1,12 @@
+﻿# mi_agente.py - Esqueleto inicial
+from entorno import Agente
+
+class MiAgente(Agente):
+    def __init__(self):
+        super().__init__(nombre="Agente Basado en Utilidad")
+    def decidir(self, percepcion):
+        # Logica inicial
+        for d in self.ACCIONES:
+            if percepcion[d] == 'meta': return d
+            if percepcion[d] == 'libre': return d
+        return 'abajo'
